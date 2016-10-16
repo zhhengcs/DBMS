55330cb1fcac5ed7dcec7f4a9fe96d72a026fb26
Jumelor <chuanwen_luo@163.com>
2016-10-16 17:52:51 +0800

    Update structfile

--- src/exp1/structfile.c
+++ src/exp1/structfile.c
@@ -1,4 +1,4 @@
-#define SIZE_PER_PAGE (8*1024)
+#define SIZE_PER_PAGE 8192
 #define Max_Table_Size 100
 //#define SIZE_BUFF 32
 //#define Page_Per_Segment 128 
@@ -6,14 +6,23 @@
 struct DataFile  
 {
 	long pageOfFile;			//	文件的大小，包含多少页 
-//	long pageOfSegment;         //   每个段的大小，包含多少页 
+  //long pageOfSegment;         //   每个段的大小，包含多少页 
     long sizePerPage;           //	每一个页的大小
 
-	long totalPage;             //总的页数
-	long pageAvail;          //当前可用的页
+  //long totalPage;             //总的页数
+	long pageAvail;             //当前可用的页数 
 	long bitMapAddr;			//	bitMap在存储文件中的地址
 	long sizeBitMap;			//	bitMap的大小,以字节为单位
 	long *data;                 //指向当前的地址	
+  
+  //空闲空间管理 
+	unsigned int freeCount;  //空闲page数  
+    long *pagefreeHead;      //空闲的头  
+    long *pagefreeTail;      //空闲的尾  
+    //long *pageusedHead;      //已使用的头  
+    //long *pageusedTail;      //已使用的尾 
+	char *pageHead;      //page的头指针,用于释放内存时候用（因为page的分配了一大块的内存)  
+    char *pageDataMemoryHead; //数据区域头指针   
 };
 
 struct mapTable                 //逻辑地址到物理地址的映射表 
